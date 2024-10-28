c417d6d0a7add21d509521ab93311263d38d92aa
buikhacnam <buikhacnam11@gmail.com>
2024-10-28 18:53:23 +0700

    first commit

---
+++ cqrs.cors/src/test/java/com/techbank/cqrs/cors/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techbank.cqrs.cors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
